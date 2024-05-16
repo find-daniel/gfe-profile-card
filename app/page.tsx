ff7e980a71355f222471961aaa607548bfc6f689
Daniel Sanchez <atlow.archive@gmail.com>
2024-05-15 22:40:50 -0700

    first attempt

--- app/page.tsx
+++ app/page.tsx
@@ -1,112 +1,84 @@
-import Image from "next/image";
+import Image from 'next/image';
+import {
+  RiGithubFill,
+  RiLinkedinBoxFill,
+  RiInstagramFill,
+  RiTwitterXFill,
+  RemixiconComponentType,
+} from '@remixicon/react';
+
+interface SocialIconLink {
+  component: RemixiconComponentType;
+  url: string;
+}
+
+const socials: SocialIconLink[] = [
+  {
+    component: RiGithubFill,
+    url: '#',
+  },
+  {
+    component: RiLinkedinBoxFill,
+    url: '#',
+  },
+  {
+    component: RiInstagramFill,
+    url: '#',
+  },
+  {
+    component: RiTwitterXFill,
+    url: '#',
+  },
+];
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
+    <main className="flex justify-center">
+      {/* Card Container */}
+      <div className="mt-[120px] flex w-[340px] flex-col items-center rounded-md bg-white px-4 py-6 text-center shadow-md">
+        {/* Profile Photo */}
+        <Image className="mb-6" src={'/profile.png'} width={64} height={64} alt="user profile photo" />
+        {/* Profile Info */}
+        <div className="mb-6">
+          {/* Name */}
+          <p className="mb-1 text-xl font-medium text-neutral-900">Sarah Dole</p>
+          {/* Job Title */}
+          <p className="text-sm font-normal text-neutral-600">Front End Engineer @ Microsoft</p>
+        </div>
+        {/* Bio */}
+        <p className="mb-10 text-base font-normal text-neutral-600">
+          I turn coffee into bugs which are fixed by someone else. Certified Stack Overflow and ChatGPT developer.
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+        {/* Contact Button */}
+        <button className="mb-6 w-full rounded-md bg-indigo-700 px-4 py-2.5 text-base font-medium text-white hover:bg-indigo-800 focus-visible:rounded-md focus-visible:bg-indigo-800 focus-visible:outline-none focus-visible:ring-4 focus-visible:ring-indigo-600/[.12] disabled:bg-neutral-100 disabled:text-neutral-400">
+          Contact me
+        </button>
+        {/* Socials */}
+        <div className="flex space-x-4">
+          {socials.map((SocialIcon, key) => (
+            <a
+              key={key}
+              href={SocialIcon.url}
+              target="_blank"
+              tabIndex={0}
+              className="cursor-pointer p-2 hover:rounded-md hover:bg-neutral-50 focus:bg-neutral-50 focus-visible:rounded-md focus-visible:outline-none focus-visible:ring-4 focus-visible:ring-indigo-600/[.12]">
+              <SocialIcon.component className="fill-indigo-700" size={20} />
+            </a>
+          ))}
         </div>
       </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
+      {/* <!-- Attribute this challenge to yourself! --> */}
+      <div className="credits" data-gfe-screenshot-exclude="true">
+        A challenge by
+        <a href="https://www.greatfrontend.com/projects?ref=challenges" target="_blank">
+          &nbsp; GreatFrontEnd Projects
         </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
+        . Built by
+        <a href="https://www.greatfrontend.com/projects/u/danieo" target="_blank">
+          &nbsp;danieo
         </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        .
       </div>
     </main>
   );
